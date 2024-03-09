e9cc30e99de8b38626cbea8d2c4bbf0d0a815a6a
Enayet <enayetflweb@gamil.com>
2024-03-09 23:16:04 +0600

    route setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import {  Navigate, Route,  Routes } from "react-router-dom"
+import { NewNote } from "./constants"
 
 
 function App() {
  
 
   return (
-   <>
-   <h1 className='text-center text-5xl font-bold text-red-500'>Hello </h1>
-   </>
+  
+  <div className="container">
+    <Routes>
+    <Route path='/' element={<h1 className="font-bold text-5xl text-red-500">Hi</h1>}/>
+    <Route path='/new' element={<NewNote />}/>
+    <Route path="/:id">
+      <Route index element={<h1>Show</h1>}/>
+      <Route path="edit" element={<h1>Edit</h1>}/>
+
+    </Route>
+    <Route path='*' element={<Navigate to='/'/>}/>
+  </Routes>
+  </div>
+  
   )
 }
 
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const NewNote = (props: Props) => {
+  return (
+    <div>NewNote</div>
+  )
+}
+
+export default NewNote
